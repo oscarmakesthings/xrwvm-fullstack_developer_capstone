4fbe7dcfaafe3042c89c6b2308319699120d0718
oscarmakesthings <oscm95073@gmail.com>
2026-04-23 10:01:32 -0400

    Add Register page

---
+++ server/frontend/src/components/Register/Register.jsx
@@ -0,0 +1,76 @@
+Ãimport React, { useState } from "react";
+
+function Register() {
+  const [formData, setFormData] = useState({
+    userName: "",
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: ""
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Registering user:", formData);
+  };
+
+  return (
+    <div style={{ width: "50%", margin: "auto", marginTop: "5%" }}>
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSubmit}>
+
+        <input
+          type="text"
+          name="userName"
+          placeholder="Username"
+          value={formData.userName}
+          onChange={handleChange}
+        /><br /><br />
+
+        <input
+          type="text"
+          name="firstName"
+          placeholder="First Name"
+          value={formData.firstName}
+          onChange={handleChange}
+        /><br /><br />
+
+        <input
+          type="text"
+          name="lastName"
+          placeholder="Last Name"
+          value={formData.lastName}
+          onChange={handleChange}
+        /><br /><br />
+
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={formData.email}
+          onChange={handleChange}
+        /><br /><br />
+
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={formData.password}
+          onChange={handleChange}
+        /><br /><br />
+
+        <button type="submit">Register</button>
+
+      </form>
+    </div>
+  );
+}
+
+export default Register;
